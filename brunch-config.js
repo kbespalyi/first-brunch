2cad4d83e5c8875d6478ba21a19f96fae5aa5599
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2016-11-14 12:49:49 -0500

    added react

--- brunch-config.js
+++ brunch-config.js
@@ -1,15 +1,20 @@
-module.exports = {
+exports.config = {
+  hot: false,
+
   files: {
     javascripts: {
+      joinTo: 'app.js',
+      /*
       joinTo: {
         'vendor.js': /^(?!app)/,
         'app.js': /^app/
       }
+      */
     },
-    stylesheets: {joinTo: 'app.css'}
+    stylesheets: { joinTo: 'app.css' }
   },
 
   plugins: {
-    babel: {presets: ['es2015']}
+    babel: { presets: ['es2015', 'react'] }
   }
 };
